83e521c584e7c424b85a90ccfafe521681bdf5b7
Bobby Wang <wbo4958@gmail.com>
2024-06-24 22:48:17 +0800

    update

--- jvm-packages/xgboost4j-spark-gpu/src/test/scala/ml/dmlc/xgboost4j/scala/spark/GpuXGBoostPluginSuite.scala
+++ jvm-packages/xgboost4j-spark-gpu/src/test/scala/ml/dmlc/xgboost4j/scala/spark/GpuXGBoostPluginSuite.scala
@@ -1,8 +1,12 @@
 package ml.dmlc.xgboost4j.scala.spark
 
+import java.io.File
+
 import scala.collection.mutable.ArrayBuffer
 
+import ai.rapids.cudf.{CSVOptions, DType, Schema, Table}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{FloatType, StructField, StructType}
 
 import ml.dmlc.xgboost4j.scala.rapids.spark.GpuTestSuite
 
@@ -224,6 +228,36 @@ class GpuXGBoostPluginSuite extends GpuTestSuite {
           //          assert(rowNumber.sum === expectedRowNum)
           assert(nonMissing.sum === expectedNonMissing)
       }
+    }
+  }
+
+  def readToQuantileDMatrix(): Unit = {
+    val cols = (0 until 12).map(v => s"f$v")
+    val label = "label"
+
+    val builder = Schema.builder()
+    (cols ++ Seq(label)).foreach(name => builder.column(DType.FLOAT32, name))
+    val schema = builder.build();
+
+    val opts = CSVOptions.builder()
+      .hasHeader().build();
+
+    val table = Table.readCSV(schema, opts, new File(getResourcePath("/veterans_lung_cancer.csv")))
+
+
+
+  }
+
+  test("XGBoost-Spark should match xgboost4j") {
+    withGpuSparkSession() { spark =>
+
+      val cols = (0 until 12).map(v => s"f$v")
+      val label = "label"
+
+      val schema = StructType((cols ++ Seq(label)).map(v => StructField(v, FloatType)))
+      val df = spark.read.schema(schema).option("header", true)
+        .csv(getResourcePath("/veterans_lung_cancer.csv"))
+      df.show()
     }
   }
 }
