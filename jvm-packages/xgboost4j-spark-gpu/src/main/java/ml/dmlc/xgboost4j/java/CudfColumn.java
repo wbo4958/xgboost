ff60e1463bca228a2a4fce7f083639e54e2a366e
Bobby <wbo4958@gmail.com>
2024-06-25 18:19:39 +0800

    supporting array

--- jvm-packages/xgboost4j-spark-gpu/src/main/java/ml/dmlc/xgboost4j/java/CudfColumn.java
+++ jvm-packages/xgboost4j-spark-gpu/src/main/java/ml/dmlc/xgboost4j/java/CudfColumn.java
@@ -16,10 +16,7 @@
 
 package ml.dmlc.xgboost4j.java;
 
-import ai.rapids.cudf.BaseDeviceMemoryBuffer;
-import ai.rapids.cudf.BufferType;
-import ai.rapids.cudf.ColumnVector;
-import ai.rapids.cudf.DType;
+import ai.rapids.cudf.*;
 
 /**
  * This class is composing of base data with Apache Arrow format from Cudf ColumnVector.
@@ -30,46 +27,63 @@ public class CudfColumn extends Column {
   private final long dataPtr; //  gpu data buffer address
   private final long shape;   // row count
   private final long validPtr; // gpu valid buffer address
+  private final long offsetPtr; // gpu offset address for list
   private final int typeSize; // type size in bytes
   private final String typeStr; // follow array interface spec
   private final long nullCount; // null count
 
   private String arrayInterface = null; // the cuda array interface
 
-  public static CudfColumn from(ColumnVector cv) {
-    BaseDeviceMemoryBuffer dataBuffer = cv.getDeviceBufferFor(BufferType.DATA);
-    BaseDeviceMemoryBuffer validBuffer = cv.getDeviceBufferFor(BufferType.VALIDITY);
+  public static CudfColumn from(ColumnVector column) {
+    ColumnView cv = column;
+    DType dType = cv.getType();
+    long nullCount = cv.getNullCount();
+    String floatType = "<f";
+    String integerType = "<i";
+    long offsetPtr = 0;
+    if (dType == DType.LIST) {
+      floatType = "<lf";
+      integerType = "<li";
+      if (cv.getOffsets() != null) {
+        offsetPtr = cv.getOffsets().getAddress();
+      }
+      cv = cv.getChildColumnView(0);
+      dType = cv.getType();
+    }
+
+    BaseDeviceMemoryBuffer dataBuffer = cv.getData();
+    BaseDeviceMemoryBuffer validBuffer = cv.getValid();
     long validPtr = 0;
     if (validBuffer != null) {
       validPtr = validBuffer.getAddress();
     }
-    DType dType = cv.getType();
     String typeStr = "";
     if (dType == DType.FLOAT32 || dType == DType.FLOAT64 ||
-        dType == DType.TIMESTAMP_DAYS || dType == DType.TIMESTAMP_MICROSECONDS ||
-        dType == DType.TIMESTAMP_MILLISECONDS || dType == DType.TIMESTAMP_NANOSECONDS ||
-        dType == DType.TIMESTAMP_SECONDS) {
-      typeStr = "<f" + dType.getSizeInBytes();
+      dType == DType.TIMESTAMP_DAYS || dType == DType.TIMESTAMP_MICROSECONDS ||
+      dType == DType.TIMESTAMP_MILLISECONDS || dType == DType.TIMESTAMP_NANOSECONDS ||
+      dType == DType.TIMESTAMP_SECONDS) {
+      typeStr = floatType + dType.getSizeInBytes();
     } else if (dType == DType.BOOL8 || dType == DType.INT8 || dType == DType.INT16 ||
-        dType == DType.INT32 || dType == DType.INT64) {
-      typeStr = "<i" + dType.getSizeInBytes();
+      dType == DType.INT32 || dType == DType.INT64) {
+      typeStr = integerType + dType.getSizeInBytes();
     } else {
       // Unsupported type.
       throw new IllegalArgumentException("Unsupported data type: " + dType);
     }
 
     return new CudfColumn(dataBuffer.getAddress(), cv.getRowCount(), validPtr,
-      dType.getSizeInBytes(), typeStr, cv.getNullCount());
+                          dType.getSizeInBytes(), typeStr, nullCount, offsetPtr);
   }
 
   private CudfColumn(long dataPtr, long shape, long validPtr, int typeSize, String typeStr,
-                    long nullCount) {
+                     long nullCount, long offsetPtr) {
     this.dataPtr = dataPtr;
     this.shape = shape;
     this.validPtr = validPtr;
     this.typeSize = typeSize;
     this.typeStr = typeStr;
     this.nullCount = nullCount;
+    this.offsetPtr = offsetPtr;
   }
 
   @Override
