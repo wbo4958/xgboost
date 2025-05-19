a5ca100e5e1d2710b28e96c16067fbf7df424ec1
Bobby Wang <wbo4958@gmail.com>
2025-05-19 21:36:40 +0800

    [jvm-packages] ExtenralMemory: Overlap the caching time

--- jvm-packages/xgboost4j-spark-gpu/src/main/scala/ml/dmlc/xgboost4j/scala/spark/ExternalMemory.scala
+++ jvm-packages/xgboost4j-spark-gpu/src/main/scala/ml/dmlc/xgboost4j/scala/spark/ExternalMemory.scala
@@ -18,10 +18,15 @@ package ml.dmlc.xgboost4j.scala.spark
 
 import java.io.File
 import java.nio.file.{Files, Paths}
+import java.util.concurrent.Executors
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
 
 import ai.rapids.cudf._
+import org.apache.commons.logging.LogFactory
 
 import ml.dmlc.xgboost4j.java.{ColumnBatch, CudfColumnBatch}
 import ml.dmlc.xgboost4j.scala.spark.Utils.withResource
@@ -61,13 +66,24 @@ private[spark] trait ExternalMemory[T] extends Iterator[Table] with AutoCloseabl
 }
 
 // The data will be cached into disk.
-private[spark] class DiskExternalMemoryIterator(val path: String) extends ExternalMemory[String] {
+private[spark] class DiskExternalMemoryIterator(val parent: String,
+                                                val cacheBatchNumber: Int = 1)
+  extends ExternalMemory[String] {
+
+  private val logger = LogFactory.getLog("XGBoostSparkGpuPlugin")
 
   private lazy val root = {
-    val tmp = path + "/xgboost"
+    val tmp = parent + "/xgboost"
     createDirectory(tmp)
     tmp
   }
+
+  logger.info(s"DiskExternalMemoryIterator createDirectory $root")
+
+  // Tasks mapping the path to the Future of caching table
+  private val taskFutures: mutable.HashMap[String, Future[Boolean]] = mutable.HashMap.empty
+  private val executor = Executors.newFixedThreadPool(2)
+  implicit val ec = ExecutionContext.fromExecutor(executor)
 
   private var counter = 0
 
@@ -79,19 +95,62 @@ private[spark] class DiskExternalMemoryIterator(val path: String) extends Extern
   }
 
   /**
+   * Cache the table into disk which runs in a separate thread
+   *
+   * @param table to be cached
+   * @param path where to cache the table
+   */
+  private def cacheTableThread(table: Table, path: String): Future[Boolean] = {
+    Future {
+      withResource(table) { _ =>
+
+        try {
+          val rows = table.getRowCount
+          val size = rows * table.getNumberOfColumns * 4 / 1024 / 1024
+          logger.info(s"cacheTableThread begin to cache table (rows: $rows, " +
+            s"size: ${size}M) to $path")
+          val names = (1 to table.getNumberOfColumns).map(_.toString)
+          val options = ArrowIPCWriterOptions.builder().withColumnNames(names: _*).build()
+          withResource(Table.writeArrowIPCChunked(options, new File(path))) { writer =>
+            writer.write(table)
+          }
+          logger.info(s"cacheTableThread Finished caching table (rows: $rows, " +
+            s"size: ${size}M) to $path ================> Done")
+          true
+        } catch {
+          case e: Throwable =>
+            throw e
+            false
+        }
+      }
+    }
+  }
+
+  /**
    * Convert the table to file path which will be cached
    *
    * @param table to be converted
    * @return the content
    */
   override def convertTable(table: Table): String = {
-    val names = (1 to table.getNumberOfColumns).map(_.toString)
-    val options = ArrowIPCWriterOptions.builder().withColumnNames(names: _*).build()
-    val path = root + "/table_" + counter + "_" + System.nanoTime();
-    counter += 1
-    withResource(Table.writeArrowIPCChunked(options, new File(path))) { writer =>
-      writer.write(table)
+    val index = counter - cacheBatchNumber
+    if (index >= 0 && index < buffers.length) {
+      checkAndWaitCachingDone(buffers(index))
+      logger.info(s"Waiting for ${buffers(index)} done")
     }
+
+    val path = root + "/table_" + counter + "_" + System.nanoTime()
+    counter += 1
+
+    val rows = table.getRowCount
+    val size = rows * table.getNumberOfColumns * 4 / 1024 / 1024
+    logger.info(s"Intend to cache table (rows: $rows, " +
+      s"size: ${size}M) to $path")
+
+    // Increase the reference count of columnars to avoid being recycled
+    val newTable = new Table((0 until table.getNumberOfColumns).map(table.getColumn): _*)
+    val future = cacheTableThread(newTable, path)
+    taskFutures += (path -> future)
     path
   }
 
@@ -106,19 +165,34 @@ private[spark] class DiskExternalMemoryIterator(val path: String) extends Extern
     }
   }
 
+  private def checkAndWaitCachingDone(path: String): Unit = {
+    val futureOpt = taskFutures.get(path)
+    if (futureOpt.isEmpty) {
+      throw new RuntimeException(s"Failed to find the caching process for $path")
+    }
+    // Wait 6s to check if the caching is done.
+    // TODO, make it configurable
+    // If timeout, it's going to throw exception
+    val success = Await.result(futureOpt.get, 6.seconds)
+    if (!success) { // Failed to cache
+      throw new RuntimeException(s"Failed to cache table to $path")
+    }
+  }
+
   /**
    * Load the path from disk to the Table
    *
-   * @param name to be loaded
+   * @param path to be loaded
    * @return Table
    */
-  override def loadTable(name: String): Table = {
-    val file = new File(name)
-    if (!file.exists()) {
-      throw new RuntimeException(s"The cache file ${name} doesn't exist" )
-    }
+  override def loadTable(path: String): Table = {
+    val file = new File(path)
+
+    logger.info(s"loadTable to table from to $path")
     try {
-      withResource(Table.readArrowIPCChunked(file)) { reader =>
+      checkAndWaitCachingDone(path)
+
+      val t = withResource(Table.readArrowIPCChunked(file)) { reader =>
         val tables = ArrayBuffer.empty[Table]
         closeOnExcept(tables) { tables =>
           var table = Option(reader.getNextIfAvailable())
@@ -135,6 +209,10 @@ private[spark] class DiskExternalMemoryIterator(val path: String) extends Extern
           tables(0)
         }
       }
+      val rows = t.getRowCount
+      val size = rows * t.getNumberOfColumns * 4 / 1024 / 1024
+      logger.info(s"loadTable done to load to table (rows: $rows, size: $size) from to $path")
+      t
     } catch {
       case e: Throwable =>
         close()
@@ -147,6 +225,7 @@ private[spark] class DiskExternalMemoryIterator(val path: String) extends Extern
   }
 
   override def close(): Unit = {
+    executor.shutdown()
     buffers.foreach { path =>
       val file = new File(path)
       if (file.exists()) {
@@ -158,8 +237,9 @@ private[spark] class DiskExternalMemoryIterator(val path: String) extends Extern
 }
 
 private[spark] object ExternalMemory {
-  def apply(path: Option[String] = None): ExternalMemory[_] = {
-    path.map(new DiskExternalMemoryIterator(_))
+  def apply(path: Option[String] = None,
+            cacheBatchNumber: Int = 1): ExternalMemory[_] = {
+    path.map(new DiskExternalMemoryIterator(_, cacheBatchNumber))
       .getOrElse(throw new RuntimeException("No disk path provided"))
   }
 }
@@ -169,7 +249,7 @@ private[spark] object ExternalMemory {
  *
  * The first round iteration gets the input batch that will be
  *   1. cached in the external memory
- *      2. fed in QuantilDmatrix
+ *   2. fed in QuantileDMatrix
  *      The second round iteration returns the cached batch got from external memory.
  *
  * @param input   the spark input iterator
@@ -177,7 +257,8 @@ private[spark] object ExternalMemory {
  */
 private[scala] class ExternalMemoryIterator(val input: Iterator[Table],
                                             val indices: ColumnIndices,
-                                            val path: Option[String] = None)
+                                            val path: Option[String] = None,
+                                            val cacheBatchNumber: Int = 1)
   extends Iterator[ColumnBatch] {
 
   private var iter = input
@@ -188,7 +269,7 @@ private[scala] class ExternalMemoryIterator(val input: Iterator[Table],
   private var inputNextIsCalled = false
 
   // visible for testing
-  private[spark] val externalMemory = ExternalMemory(path)
+  private[spark] val externalMemory = ExternalMemory(path, cacheBatchNumber)
 
   override def hasNext: Boolean = {
     val value = iter.hasNext
