a3e5e809b294d50845156a0e6184737a6f199f42
Bobby Wang <wbo4958@gmail.com>
2025-05-22 15:08:12 +0800

    update

--- jvm-packages/xgboost4j-spark-gpu/src/main/scala/ml/dmlc/xgboost4j/scala/spark/ExternalMemory.scala
+++ jvm-packages/xgboost4j-spark-gpu/src/main/scala/ml/dmlc/xgboost4j/scala/spark/ExternalMemory.scala
@@ -83,7 +83,7 @@ private[spark] class DiskExternalMemoryIterator(val parent: String,
   // Tasks mapping the path to the Future of caching table
   private val taskFutures: mutable.HashMap[String, Future[Boolean]] = mutable.HashMap.empty
   private val executor = Executors.newFixedThreadPool(2)
-  implicit val ec = ExecutionContext.fromExecutor(executor)
+  private val ec = ExecutionContext.fromExecutor(executor)
   private var cachingDone = false
   private var loadedCounter = 0
   private val preloadFutures: mutable.HashMap[String, Future[Table]] = mutable.HashMap.empty
@@ -127,7 +127,7 @@ private[spark] class DiskExternalMemoryIterator(val parent: String,
             false
         }
       }
-    }
+    }(ec)
   }
 
   /**
@@ -244,7 +244,7 @@ private[spark] class DiskExternalMemoryIterator(val parent: String,
         val index = loadedCounter
         if (index < counter) {
           logger.info(s"Preload Preload begins to preload from ${buffers(index)}")
-          taskFutures += (path -> Future {loadTableFromDisk(buffers(index))})
+          taskFutures += (path -> Future {loadTableFromDisk(buffers(index))}(ec))
         }
       }
       t
